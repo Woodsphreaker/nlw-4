e25d1d8850970755312cd578a67265e77d8084b0
Carlo Enrico <carlo.enrico@uol.com.br>
2021-02-25 23:42:25 -0300

    chore: add countdown button

--- next-app/src/components/Countdown/index.tsx
+++ next-app/src/components/Countdown/index.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
 import { Container, CountdownContainer, Counter, Separator, Button } from './styles';
 
+const splitTime = (time) => {
+  return time.toString().padStart(2, '0').split('')
+}
+
 const Countdown: React.FC = () => {
+  const [time, setTime] = useState(25 * 60)
+  const [counterActive, setCounterActive] = useState(false)
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+
+  const [minuteLeft, minuteRight] = splitTime(minutes)
+  const [secondLeft, secondRight] = splitTime(seconds)
+
+  const changeConterStatus = () => {
+    setCounterActive((prevState) => !prevState )
+  }
+
+  useEffect(() => {
+    if(counterActive && time > 0) {
+      setTimeout(() => setTime((prevState) => prevState - 1) , 1000)
+    }
+  }, [counterActive, time]);
+  
   return (
     <Container>
       <CountdownContainer>
         <Counter>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </Counter>
         <Separator>
           :
         </Separator>
         <Counter>
-          <span>2</span>
-          <span>5</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </Counter>
       </CountdownContainer>
 
-      <Button>
-        Iniciar um ciclo
+      <Button onClick={changeConterStatus}>
+        {counterActive ? 'Pausar ciclo atual' : 'Iniciar um ciclo'} 
       </Button>
     </Container>
   )
